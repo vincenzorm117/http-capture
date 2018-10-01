c277a779eabb3ad119967d2239a596dcfc6c3f49
Vincenzo Marconi <vincenzorm117@gmail.com>
2018-09-30 22:11:39 -0400

    Rebuilt server to save raw request and accept PORT environment variable.

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 
+var PORT = process.env.PORT || 3000
 
-const express = require('express')
-const app = express()
+const net = require('net')
 const fs = require('fs')
 
+if( !fs.existsSync('./payloads') ) {
+    fs.mkdirSync('./payloads');
+}
+
+var server = net.createServer((sock) => {
+    console.log(`Connected: ${sock.remoteAddress}`)
+    let filename = FileName(sock);
+    let wstream = fs.createWriteStream(`./payloads/${filename}`);
 
 
-app.use((req, res, next) => {
-    req.rawBody = '';
-    req.setEncoding('utf8');
-    req.on('data', (chunk) => {  req.rawBody += chunk; });
-    req.on('end', () => { next(); });
+    sock.on('data', wstream.write.bind(wstream));
+    sock.on('end', () => {
+        wstream.end();
+        delete wstream;
+        console.log(`Disconnected: ${sock.remoteAddress}`)
+    });
+    
+    setTimeout(() => {
+        if( !sock.destroyed ) {
+            sock.write('HTTP/1.1 200 OK\r\n');
+            sock.end();
+        }
+    }, 3000);
 });
 
+server.listen(PORT, 'localhost');
 
-app.all('*', async (req, res) => {
+
+
+function FileName() {
     var d = new Date(),
 		year = d.getFullYear(),
 		month = d.getMonth(),
@@ -24,66 +43,8 @@ app.all('*', async (req, res) => {
         seconds = d.getSeconds();
     if( month < 10 ) month = '0' + month;
     if( date < 10 ) date = '0' + date;
-
-    let folderName = `${year}_${month}_${date}_${hour}_${minutes}_${seconds}`;
-    let params_filename = `${folderName}/params.json`;
-    let body_filename = `${folderName}/body.txt`;
-    let headers_filename = `${folderName}/headers.json`;
-    let other_filename = `${folderName}/other.json`;
-
-    if( !fs.existsSync(`./payloads/${folderName}`) ) {
-        fs.mkdirSync(`./payloads/${folderName}`);
-    }
-
-    let promises = [];
-
-    promises.push(new Promise((resolve,reject) => {
-        fs.writeFile(`./payloads/${body_filename}`, req.rawBody, (err) => {
-            if( err ) reject(err);
-            return resolve();
-        });
-    }));
-
-    promises.push(new Promise((resolve,reject) => {
-        let headers = JSON.stringify(req.headers);
-        fs.writeFile(`./payloads/${headers_filename}`, headers, (err) => {
-            if( err ) reject(err);
-            return resolve();
-        });
-    }));
-
-    promises.push(new Promise((resolve,reject) => {
-        let obj = {
-            method: req.method,
-            protocol: req.protocol,
-            host: req.host,
-            hostname: req.hostname,
-            path: req.path,
-            originalUrl: req.originalUrl,
-            baseUrl: req.baseUrl,
-            url: req.url,
-            ip: req.ip,
-            ips: req.ips,
-            httpVersion: req.httpVersion,
-            query: req.query,
-            params: req.params
-        };
-        fs.writeFile(`./payloads/${other_filename}`, JSON.stringify(obj), (err) => {
-            if( err ) reject(err);
-            return resolve();
-        });
-    }));
-
-    Promise.all(promises)
-        .then(() => {
-            return res.status(200).send();
-        })
-        .catch((error) => {
-            console.log(error);
-            return res.status(500).json({ message: "Server error contact Admin." })
-        })
-});
-
-
-
-app.listen(3000, () => console.log('Gator app listening on port 3000!'));
+    if( hour < 10 ) hour = '0' + hour;
+    if( minutes < 10 ) minutes = '0' + minutes;
+    if( seconds < 10 ) seconds = '0' + seconds;
+    return `request__${year}-${month}-${date}__${hour}-${minutes}-${seconds}.http`;
+}
